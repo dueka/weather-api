82c9627ef4413f5a79afd569af4ae00b33339126
dueka <ekanemd@live.com>
2020-07-07 12:38:31 +0100

    added the responseweather string

--- Weather/src/source/Weather.js
+++ Weather/src/source/Weather.js
@@ -3,6 +3,28 @@ import { Current } from "../formats/current";
 import { get } from "https";
 import { IncomingMessage } from "http";
 
+const weatherKey = process.env.ACCESS_KEY;
+const requestURL = new URL("http://api.weatherstack.com/current");
+requestURL.searchParams.append("access_key", weatherKey);
+
+const [, , ...requestArgs] = process.argv;
+let args = fp.join(" ")(requestArgs);
+args = fp.split(/, ?/)(args);
+
+const responseWeather = (responseObj) => {
+  const respLocation = responseObj.location;
+  const respCurrent = responseObj.current;
+  const zipCode =
+    responseObj.request.type == "Zipcode" ? `${responseObj.request.query}` : "";
+  const Country = `${respLocation.name}, ${respLocation.country}${zipCode}`;
+  const responseTime = `The time in ${Country} is ${respLocation.observation_time}`;
+  const weatherString =
+    `The Weather as of ${respCurrent.observation_time}:` +
+    `${fp.join(", ")(respCurrent.weather_descriptions)}` +
+    ` with temperature of ${respCurrent.temperature}c`;
+  return `\n${responseTime}\n${weatherString}`;
+};
+
 // const params = {
 //   access_key: "fc82b2e65b6e76965c53699204031ca4",
 //   query: "New York",
