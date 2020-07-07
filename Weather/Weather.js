c93393c604e6856dc088d4205670b06f17e5f478
dueka <ekanemd@live.com>
2020-07-07 09:29:50 +0100

    Added the query selectors for the weather

---
+++ Weather/Weather.js
@@ -0,0 +1,6 @@
+var input = document.querySelector(".input_text");
+var main = document.querySelector("#name");
+var temp = document.querySelector(".temp");
+var time = document.querySelector("time");
+var desc = document.querySelector(".description");
+var button = document.querySelector(".submit");
